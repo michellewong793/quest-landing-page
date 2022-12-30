f86ea1a2e4e81415a3342658f97612ccb1ce67f9
michellewong793 <michellewong793@gmail.com>
2022-12-30 14:26:26 -0800

    make mobile nav work

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,21 +1,25 @@
 import '../styles/globals.css'
 import Layout from "../components/layout";
+import {Press_Start_2P, Lexend} from '@next/font/google'
 
 
-import {Lexend} from '@next/font/google';
-
-const lexend = Lexend({
-   subsets: ['latin'],
-  //  display: "fallback",
-  })
-
+const pressPlayFont = Press_Start_2P({ subsets: ['latin'], weight: '400' })
+const lexend = Lexend({subsets: 'latin', weight: '300'})
 
 
 function MyApp({ Component, pageProps }) {
   return (
-  <main className={lexend.className}>
+    <>
+    <style jsx global>{`
+    a {
+      font-family: ${pressPlayFont.style.fontFamily};
+    }
+    p {
+      font-family: ${lexend.style.fontFamily}
+    }
+  `}</style>
   <Component {...pageProps} />
-  </main>
+  </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../components/layout";
+import Nav from "../components/Nav";
 import React, { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
@@ -10,19 +10,10 @@ import {Lexend} from '@next/font/google';
 // If loading a variable font, you don't need to specify the font weight
 const lexend = Lexend({
    subsets: ['latin'],
-  //  display: "fallback",
   })
 
 
-const title = {
-  color: "#EF83F8",
-  fontSize: "4rem",
-  fontFamily: "puffin-arcade-chrome",
-  marginLeft: '3rem',
-  marginTop: "2.3rem",
-  float: 'left',
-  position: 'relative',
-};
+
 
 
 
@@ -30,23 +21,20 @@ export default function Home() {
   return (
     <>
     <Head>
-        <title>send love to the homies who need it</title>
-        <meta name="description" content="quit now, bro" />
+        <title>Quest</title>
+        <meta name="description" content="Quest is the world's first enterprise tool for crowdsourcing and monetizing data with individuals through the power of Artifical intelligence and zero knoweldge proofs." />
         <link rel="icon" href="/favicon.ico" />
     </Head>
     <div className={styles.container}>
-      <h1 style={title}>
-          Quest
-      </h1>
-      <Layout/> 
+     <Nav/>
 
     <div>
       <h1 className={`${lexend.className} ${styles.header}`}>
-      Quest is the world’s first ecosystem for community-driven crowdsourcing and monetization of data.
+      Quest is the world’s first ecosystem for community-driven crowdsourcing and monetization of data. 
       </h1>
-      <h3 className={styles.subheader}>
-      We build technologies that solve collective action problems in ways that were not thought to be possible before.
-      </h3>
+      <p className={styles.subheader}>
+      We believe that individuals should have control over their own data and have the opportunity to profit from it. Our mission is to empower individuals to profit from their own data by providing a platform for them to securely and safely share their data with companies who need it.  By empowering individuals to profit from their own data, we aim to create a more equitable and sustainable economy where individuals are fairly compensated for their contributions.
+      </p>
     </div>  
     </div>
     </>
