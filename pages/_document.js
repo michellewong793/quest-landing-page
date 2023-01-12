7d264b057df79404ba2a0878cc493a09d6f129d2
michellewong793 <michellewong793@gmail.com>
2023-01-11 18:28:53 -0800

    Add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,13 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="stylesheet" href="https://use.typekit.net/uov6xbj.css"></link>
+                    <meta property="og:title" content="Quest Ecosystem"/>
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:image" content="/Quest_Pink_Logo.svg"/>
+                    <meta name="twitter:card" content="summary_large_image"/>
+<meta name="twitter:site" content="@ourquest_xyz"/>
+<meta name="twitter:creator" content="@ourquest_xyz"/>
+<meta name="twitter:image" content="https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg"></meta>
                 </Head>
                 <body>
                     <Main />
